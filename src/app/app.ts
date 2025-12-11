322bff0d4d84b8d931ac070777485e2e77ea0765
focarica <a.vinicius0042@gmail.com>
2025-12-11 20:51:34 -0300

    initial style(header and subheader) for the plain page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Router, RouterModule, } from '@angular/router';
+
+interface NavItem {
+  label: string;
+  route: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
+  navItems: NavItem[] = [
+    { label: 'Home', route: '/home' },
+    { label: 'CV', route: '/cv' },
+    { label: 'find me', route: '/find-me' },
+    { label: 'about', route: '/about' }
+  ];
+
+  constructor(private router: Router) {}
+
+  navigate(route: string): void {
+    this.router.navigate([route]);
+  }
 }
