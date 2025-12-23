7464bee51d33fd400f225dfc9dbbb1f3d30fb900
focarica <a.vinicius0042@gmail.com>
2025-12-23 12:25:51 -0300

    created post page and added its dependencies

--- src/app/features/post/post.component.ts
+++ src/app/features/post/post.component.ts
@@ -1,24 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
-import { Router, RouterModule, } from '@angular/router';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule, } from '@angular/router';
 import { HttpService } from '../../http.service';
+import { Post } from '../../shared/models/Post';
+import { Subscription } from 'rxjs';
+import { Location } from '@angular/common';
+import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-post-page',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MarkdownModule],
   templateUrl: './post.html',
 })
 export class PostPageComponent implements OnInit{
 
   private httpService = inject(HttpService)
+  private location = inject(Location);
+  private sub?: Subscription;
+  
+  post = signal<Post | null>(null);
 
-  constructor(private router: Router) {}
-
+  constructor(private router: Router, private route: ActivatedRoute) {
+  }
+  
   navigate(route: string): void {
     this.router.navigate([route]);
   }
 
   ngOnInit(): void {
+    this.sub = this.route.paramMap.subscribe(params => {
+      const id = params.get('id');
+
+      if (id) {
+        this.getPost(id);
+      }
+    });
   }
 
+  goBack() {
+    this.location.back();
+  } 
+
+  getPost(id: string){
+    this.httpService.findPost(id).subscribe(
+      response => {
+        this.post.set(response)
+      }
+    )
+  }
 }
