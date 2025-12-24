22d33a14b495ff65283374660b0a03c409f065f2
focarica <a.vinicius0042@gmail.com>
2025-12-24 20:40:29 -0300

    back button going to home and not last page visited

--- src/app/features/post/post.component.ts
+++ src/app/features/post/post.component.ts
@@ -4,7 +4,6 @@ import { ActivatedRoute, Router, RouterModule, } from '@angular/router';
 import { HttpService } from '../../http.service';
 import { Post } from '../../shared/models/Post';
 import { Subscription } from 'rxjs';
-import { Location } from '@angular/common';
 import { MarkdownComponent } from 'ngx-markdown';
 
 @Component({
@@ -15,7 +14,6 @@ import { MarkdownComponent } from 'ngx-markdown';
 export class PostPageComponent implements OnInit{
 
   private httpService = inject(HttpService)
-  private location = inject(Location);
   private sub?: Subscription;
   
   post = signal<Post | null>(null);
@@ -36,10 +34,6 @@ export class PostPageComponent implements OnInit{
       }
     });
   }
-
-  goBack() {
-    this.location.back();
-  } 
 
   getPost(id: string){
     this.httpService.findPost(id).subscribe(
