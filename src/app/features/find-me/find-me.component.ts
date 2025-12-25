9d9f1cb0a22216e75a29a6eeb3540cffda593a87
focarica <a.vinicius0042@gmail.com>
2025-12-25 11:36:52 -0300

    implemente find-me screen

--- src/app/features/find-me/find-me.component.ts
+++ src/app/features/find-me/find-me.component.ts
@@ -2,15 +2,53 @@ import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { Router, RouterModule, } from '@angular/router';
 import { HttpService } from '../../http.service';
+import { FIND_ME_ICONS } from '../../shared/icons/find-me/find-me-icons';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
+
+interface FindMeLink {
+  label: string;
+  url: string;
+  hint?: string;
+  icon: keyof typeof FIND_ME_ICONS;
+}
 @Component({
   selector: 'app-find-me-page',
   imports: [CommonModule, RouterModule],
   templateUrl: './find-me.html',
 })
 export class FindMeComponent implements OnInit{
+  icons: Record<string, SafeHtml> = {};
+  private sanitizer = inject(DomSanitizer);
 
-  private httpService = inject(HttpService)
+  links: FindMeLink[] = [
+    {
+      label: 'GitHub',
+      url: 'https://github.com/focarica',
+      hint: 'projects, experiments, open source',
+      icon: 'github'
+    },
+    {
+      label: 'LinkedIn',
+      url: 'https://linkedin.com/in/artvniss',
+      hint: 'professional background',
+      icon: 'linkedin'
+    },
+    {
+      label: 'Email',
+      url: 'mailto:a.vinicius0042@gmail.com',
+      hint: 'direct contact',
+      icon: 'email'
+    },
+    {
+      label: 'Twitter / X',
+      url: 'https://x.com/focarica',
+      hint: 'twitter & short thoughts',
+      icon: 'twitter'
+    }
+  ];
+
+
 
   constructor(private router: Router) {}
 
@@ -19,6 +57,11 @@ export class FindMeComponent implements OnInit{
   }
 
   ngOnInit(): void {
+    for (const key in FIND_ME_ICONS) {
+      this.icons[key] = this.sanitizer.bypassSecurityTrustHtml(
+        FIND_ME_ICONS[key]
+      );
+    }
   }
 
 }
